928c648d4c61f3a061f6cf0a827a4044a085a067
kgtest <dtgkhang99@gmail.com>
2022-10-02 15:38:40 +0700

    0.1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { AiFillInstagram } from 'react-icons/ai'
+import { BsFacebook, BsLinkedin } from 'react-icons/bs'
 import './footer.css'
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>DTGK</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Psortfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+      <a href="https://www.linkedin.com/in/khang-do-b38456204/" target="_blank"><BsLinkedin/></a>
+        <a href="https://instagram.com/dtgkhang"><AiFillInstagram/></a>
+        <a href="https://www.facebook.com/khang.do.5623/"><BsFacebook/></a>
+      </div>
+    </footer>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import DTGK from '../../assets/dtgk.png'
-import HeaderSocial from './HeaderSocials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import DTGK from "../../assets/dtgk3.png";
+import HeaderSocial from "./HeaderSocials";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Khang Do</h1>
-        <h5 className="text-light">Front-end Developer</h5>
-        <CTA/>
-        <HeaderSocial/> 
+        <div class="wrapper">
+          <ul class="dynamic-txts">
+            <li>
+              <span>Khang Do</span>
+            </li>
+            
+          </ul>
+          
+        </div>
+              <h5>Front-end Developer</h5>
+          
+        <CTA />
+        <HeaderSocial />
         <div className="me">
           <img src={DTGK} alt="me" />
         </div>
 
-        <a href="#contact" className='scroll__down'>Sroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Sroll Down
+        </a>
       </div>
-    </header>    
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
