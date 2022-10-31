bd25d8e6b3af8c1cdee3e03669cf9bdcd804b034
kgtest <dtgkhang99@gmail.com>
2022-10-31 14:28:17 +0700

    cx

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import React, { Component }  from 'react';
 
 import App from './App'
 import './index.css'
+import { getElementError } from "@testing-library/react";
 
 ReactDOM.render(<App/>,document.querySelector("#root"));
+document.getElementsByClassName
